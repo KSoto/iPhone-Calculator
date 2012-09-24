9d65c8c0725a3f2d07bc593c69adf9003080649d
KSoto <KReneSoto@gmail.com>
2012-09-24 03:09:54 +0100

    All buttons and labels now associated with class variables and class methods.

--- Soto.Katie_asgt3/ViewController.h
+++ Soto.Katie_asgt3/ViewController.h
@@ -8,6 +8,79 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITextFieldDelegate>
+{
+    
+@private
+    //label that will display the operation (+, -, etc.) aligned left
+    IBOutlet UILabel* _operationLabel;
+    //label that will display the operand (1, 5, etc.) aligned right
+    IBOutlet UILabel* _operandLabel;
+    
+    //all buttons
+    IBOutlet UIButton* _n1_button;
+    IBOutlet UIButton* _n2_button;
+    IBOutlet UIButton* _n3_button;
+    IBOutlet UIButton* _n4_button;
+    IBOutlet UIButton* _n5_button;
+    IBOutlet UIButton* _n6_button;
+    IBOutlet UIButton* _n7_button;
+    IBOutlet UIButton* _n8_button;
+    IBOutlet UIButton* _n9_button;
+    IBOutlet UIButton* _n0_button;
+    IBOutlet UIButton* _decimal_button;
+    IBOutlet UIButton* _neg_pos_button;
+    IBOutlet UIButton* _equal_button;
+    IBOutlet UIButton* _subtract_button;
+    IBOutlet UIButton* _addition_button;
+    IBOutlet UIButton* _division_button;
+    IBOutlet UIButton* _multipy_button;
+    IBOutlet UIButton* _root_button;
+    IBOutlet UIButton* _clear_button;
+}
+
+@property (nonatomic, retain) UILabel* operationLabel;
+@property (nonatomic, retain) UILabel* operandLabel;
+
+@property (nonatomic, retain) UIButton* n1_button;
+@property (nonatomic, retain) UIButton* n2_button;
+@property (nonatomic, retain) UIButton* n3_button;
+@property (nonatomic, retain) UIButton* n4_button;
+@property (nonatomic, retain) UIButton* n5_button;
+@property (nonatomic, retain) UIButton* n6_button;
+@property (nonatomic, retain) UIButton* n7_button;
+@property (nonatomic, retain) UIButton* n8_button;
+@property (nonatomic, retain) UIButton* n9_button;
+@property (nonatomic, retain) UIButton* n0_button;
+@property (nonatomic, retain) UIButton* decimal_button;
+@property (nonatomic, retain) UIButton* neg_pos_button;
+@property (nonatomic, retain) UIButton* equal_button;
+@property (nonatomic, retain) UIButton* subtract_button;
+@property (nonatomic, retain) UIButton* addition_button;
+@property (nonatomic, retain) UIButton* division_button;
+@property (nonatomic, retain) UIButton* multipy_button;
+@property (nonatomic, retain) UIButton* root_button;
+@property (nonatomic, retain) UIButton* clear_button;
+
+-(IBAction)n1_action :(UIButton*)sender;
+-(IBAction)n2_action :(UIButton*)sender;
+-(IBAction)n3_action :(UIButton*)sender;
+-(IBAction)n4_action :(UIButton*)sender;
+-(IBAction)n5_action :(UIButton*)sender;
+-(IBAction)n6_action :(UIButton*)sender;
+-(IBAction)n7_action :(UIButton*)sender;
+-(IBAction)n8_action :(UIButton*)sender;
+-(IBAction)n9_action :(UIButton*)sender;
+-(IBAction)n0_action :(UIButton*)sender;
+-(IBAction)decimal_action :(UIButton*)sender;
+-(IBAction)neg_pos_action :(UIButton*)sender;
+-(IBAction)equal_action :(UIButton*)sender;
+-(IBAction)subtract_action :(UIButton*)sender;
+-(IBAction)addition_action :(UIButton*)sender;
+-(IBAction)division_action :(UIButton*)sender;
+-(IBAction)multiply_action :(UIButton*)sender;
+-(IBAction)root_action :(UIButton*)sender;
+-(IBAction)clear_action :(UIButton*)sender;
+
 
 @end
--- Soto.Katie_asgt3/ViewController.m
+++ Soto.Katie_asgt3/ViewController.m
@@ -14,6 +14,127 @@
 
 @implementation ViewController
 
+@synthesize operationLabel = _operationLabel;
+@synthesize operandLabel = _operandLabel;
+
+@synthesize n1_button = _n1_button;
+@synthesize n2_button = _n2_button;
+@synthesize n3_button = _n3_button;
+@synthesize n4_button = _n4_button;
+@synthesize n5_button = _n5_button;
+@synthesize n6_button = _n6_button;
+@synthesize n7_button = _n7_button;
+@synthesize n8_button = _n8_button;
+@synthesize n9_button = _n9_button;
+@synthesize n0_button = _n0_button;
+@synthesize decimal_button = _decimal_button;
+@synthesize neg_pos_button = _neg_pos_button;
+@synthesize equal_button = _equal_button;
+@synthesize subtract_button = _subtract_button;
+@synthesize addition_button = _addition_button;
+@synthesize division_button = _division_button;
+@synthesize multipy_button = _multipy_button;
+@synthesize root_button = _root_button;
+@synthesize clear_button = _clear_button;
+
+
+
+-(IBAction)n1_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n2_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n3_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n4_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n5_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n6_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n7_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n8_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n9_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)n0_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)decimal_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)neg_pos_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)equal_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)subtract_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)addition_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)division_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)multiply_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)root_action :(UIButton*)sender
+{
+    
+}
+
+-(IBAction)clear_action :(UIButton*)sender
+{
+    
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
